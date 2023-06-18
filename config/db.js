0d0efb3481828e930cf942afa87235f823e9dd4c
VanshVg <vansh.vg18@gmail.com>
2023-06-18 18:35:12 +0530

    Database change

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb://127.0.0.1:27017/bookDirectory";
+const url =
+  "mongodb+srv://gandhiv328:Thl8M550smCGzrD1@cluster0.wultdvc.mongodb.net/";
 
 module.exports = mongoose.connect(url);
